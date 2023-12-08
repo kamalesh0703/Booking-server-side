2ce66fef4ff231e4b8d12dcad88de434ba951781
kamalesh0703 <kamaleshponmani0703@gmail.com>
2023-12-08 21:27:15 +0530

    01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-
-function App() {
+import Home from './Components/Home/Home';
+import List from './Components/List/List';
+import Hotel from './Components/Hotel/Hotel';   
+import Signup from './Components/Signup/Signup'; 
+import NavBar from './Components/Navbar/NavBar'; 
+function App() {   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path='/'  element={<Home/>} />
+          <Route path='/hotels'  element={<List/>} />
+          {/* <Route path='/services' element={<List/>} /> */}
+          <Route path='/hotels/:id' element={<Hotel/>} />
+          <Route path='/signup' element={<Signup/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Navbar/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+function NavBar() {
+  return (
+    <div className='navbar-container'>
+        <div className="company-logo">
+
+        </div>
+       <ul className='nav-lists'>
+        <li>
+            
+        </li>
+       </ul>
+    </div>
+  )
+}
+
+export default NavBar
